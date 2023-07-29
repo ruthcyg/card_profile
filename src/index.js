80a5f5fb2ca84c753bf41bf6405361991947390e
ruthcyg <ewho.ruth2014@gmail.com>
2023-07-29 17:12:37 +0530

    first challenge

--- src/index.js
+++ src/index.js
@@ -1,10 +1,82 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import profileData from "./profile";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import reportWebVitals from "./reportWebVitals";
+
+function App() {
+  
+  return (
+    <div className="card">
+      <div className="data">
+        <Avatar />
+        <Intro />
+        <SkillList />
+       
+      </div>
+    </div>
+  );
+}
+
+// function App() {
+//   return (
+//     <div className="container">
+//       <Header />
+//       <Profile />
+//       <Footer />
+//     </div>
+//   );
+// }
+
+// function Header() {
+//   return (
+//     <header className="header">
+//       <h1>Porfiolio</h1>
+//     </header>
+//   );
+// }
+
+function Avatar() {
+  return (
+  <img className ="avatar"src="profilepic/ruthcy.jpg" alt="Ruth Ewho"/>
+  );
+}
+
+function Intro(props) {
+  return (
+    <div className="data">
+      <h1>Ruth Ewho</h1>
+      <p>
+        I am highly motivated and enthusiastic full-stack developer with a
+        strong foundation in web development. With a passion for technology and
+        a dedication
+      </p>
+    </div>
+  );
+}
+
+function SkillList() {
+  return (
+    <div className="skill-list">
+      <Skills skill="Python" emoji = "🚀" color = "orange" />
+      <Skills skill = "HTML" emoji = "⭐" color = "red" />
+      <Skills skill = "Node Js" emoji = "🌈"  color = "green"/>
+      <Skills skill = " React Native"  emoji = "👌" color = "purple"/>
+    </div>
+  );
+}
+
+function Skills(props) {
+  return (
+    <div className="skill" style={{ backgroundColor: props.color }}>
+      <span>{props.skill}</span>
+      <span>{props.emoji}</span>
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
---
+++ src/profile.js
@@ -0,0 +1,12 @@
+const profileData = [
+  {
+    name: "Ewho Ruth",
+    age: "30 years",
+    job: "freelancer",
+    description: "full stack Developer",
+    hobbies: "Dancing and Singing",
+    photoName: "profilepic/ruthcy.jpg",
+  },
+];
+
+export default profileData;
